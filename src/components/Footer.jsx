df4b5906e4e56a0083b616f6e6a57cb9f8f9c118
vais2004 <vkawale2004@gmail.com>
2025-06-03 12:42:23 +0530

    update: footer Links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,17 +5,39 @@ export default function Footer() {
   return (
     <>
       <footer className="bg-light py-2 text-center">
-        {/* <div className="container py-3 d-flex justify-content-between align-items-center"> */}
         <p className="mb-0 mx-auto text-center">
           &copy; MyStyleSpot. All rights reserved.
         </p>
         <br />
         <div className="pb-3">
-          <i onClick={() => navigate("/")} className="bi bi-facebook px-2"></i>
-          <i onClick={() => navigate("/")} className="bi bi-twitter-x px-2"></i>
-          <i onClick={() => navigate("/")} className="bi bi-instagram px-2"></i>
-          <i onClick={() => navigate("/")} className="bi bi-youtube px-2"></i>
-          {/* </div> */}
+          <a
+            className="text-black"
+            href="https://www.facebook.com/login.php/"
+            target="_blank"
+            rel="noopener noreferrer">
+            <i className="bi bi-facebook px-2"></i>
+          </a>
+          <a
+            className="text-black"
+            href="https://x.com/i/flow/login"
+            target="_blank"
+            rel="noopener noreferrer">
+            <i className="bi bi-twitter-x px-2"></i>
+          </a>
+          <a
+            className="text-black"
+            href="https://www.instagram.com/accounts/login/"
+            target="_blank"
+            rel="noopener noreferrer">
+            <i className="bi bi-instagram px-2"></i>
+          </a>
+          <a
+            className="text-black"
+            href="https://www.youtube.com/"
+            target="_blank"
+            rel="noopener noreferrer">
+            <i className="bi bi-youtube px-2"></i>
+          </a>
         </div>
       </footer>
     </>
